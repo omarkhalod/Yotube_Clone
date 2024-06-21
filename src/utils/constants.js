6169d079810c72b70c3d892fa5b34a93247933d6
Omar Mohamed <omarriad555@gmail.com>
2024-06-21 19:02:15 +0300

    update README.md

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
+  //{ name: "JS Mastery", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "Leauge of Legends", icon: <SportsEsportsIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
